55ae8d0207a75fe4c05036dfda46db69078df41a
Saiyeshwanth-sketch <64353395+Saiyeshwanth-sketch@users.noreply.github.com>
2025-08-01 00:07:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,62 +5,98 @@ const products = [
   {
     name: "Crimson Rose Rakhi",
     price: 40,
-    images: ["https://i.ibb.co/6J8LkK3L/Untitled-2.jpg", "https://i.ibb.co/6cMbxCRq/IMG-20250726-WA0005-2.jpg"]
+    images: [
+      "https://i.ibb.co/6J8LkK3L/Untitled-2.jpg",
+      "https://i.ibb.co/6cMbxCRq/IMG-20250726-WA0005-2.jpg"
+    ]
   },
   {
     name: "Round Radiance Rakhi",
     price: 29,
-    images: ["https://i.ibb.co/MyJGcdvb/Untitled-1.png", "https://i.ibb.co/C33Wx31C/PXL-20250719-141726150.jpg"]
+    images: [
+      "https://i.ibb.co/MyJGcdvb/Untitled-1.png",
+      "https://i.ibb.co/C33Wx31C/PXL-20250719-141726150.jpg"
+    ]
   },
   {
     name: "Subhadra Rakhi for Krishna",
     price: 35,
-    images: ["https://i.ibb.co/7JCLq3NZ/Untitled.png", "https://i.ibb.co/GvD10bPX/IMG-20250719-183903112-HDR-2.jpg"]
+    images: [
+      "https://i.ibb.co/7JCLq3NZ/Untitled.png",
+      "https://i.ibb.co/GvD10bPX/IMG-20250719-183903112-HDR-2.jpg"
+    ]
   },
   {
     name: "Mayur Rakhi",
     price: 49,
-    images: ["https://i.ibb.co/7xRhBnfw/Untitled-3.png", "https://i.ibb.co/k2RDXmkP/IMG-20250719-194159554-HDR.jpg"]
+    images: [
+      "https://i.ibb.co/7xRhBnfw/Untitled-3.png",
+      "https://i.ibb.co/k2RDXmkP/IMG-20250719-194159554-HDR.jpg"
+    ]
   },
   {
     name: "Morpankh Rakhi",
     price: 60,
-    images: ["https://i.ibb.co/1YwnLq49/Untitled-1-4.jpg", "https://i.ibb.co/PZB17wbx/IMG-20250714-WA0059.jpg"]
+    images: [
+      "https://i.ibb.co/1YwnLq49/Untitled-1-4.jpg",
+      "https://i.ibb.co/PZB17wbx/IMG-20250714-WA0059.jpg"
+    ]
   },
   {
     name: "Golden Petal Rakhi",
     price: 49,
-    images: ["https://i.ibb.co/3mbWpc5t/Untitled.jpg", "https://i.ibb.co/zV1P9yzJ/IMG-20250719-194010737-HDR.jpg"]
+    images: [
+      "https://i.ibb.co/3mbWpc5t/Untitled.jpg",
+      "https://i.ibb.co/zV1P9yzJ/IMG-20250719-194010737-HDR.jpg"
+    ]
   },
   {
     name: "Golden Heart Rakhi",
     price: 69,
-    images: ["https://i.ibb.co/XfCq0xs6/Untitled-1.jpg", "https://i.ibb.co/27bPD7mq/IMG-20250714-WA0043.jpg"]
+    images: [
+      "https://i.ibb.co/XfCq0xs6/Untitled-1.jpg",
+      "https://i.ibb.co/27bPD7mq/IMG-20250714-WA0043.jpg"
+    ]
   },
   {
     name: "Golden Om Charm Rakhi",
     price: 60,
-    images: ["https://i.ibb.co/N623mj3F/Untitled-1-3.jpg", "https://i.ibb.co/JW09xL71/IMG-20250714-WA0078.jpg"]
+    images: [
+      "https://i.ibb.co/N623mj3F/Untitled-1-3.jpg",
+      "https://i.ibb.co/JW09xL71/IMG-20250714-WA0078.jpg"
+    ]
   },
   {
     name: "Big Bro Rakhi",
     price: 0,
-    images: ["https://i.ibb.co/PvBVWKDc/Untitled-2.jpg", "https://i.ibb.co/ksSGrhs5/IMG-20250719-192946382-HDR.jpg"]
+    images: [
+      "https://i.ibb.co/PvBVWKDc/Untitled-2.jpg",
+      "https://i.ibb.co/ksSGrhs5/IMG-20250719-192946382-HDR.jpg"
+    ]
   },
   {
     name: "Telugu Brother Rakhi",
     price: 45,
-    images: ["https://i.ibb.co/9k5Bn7T3/IMG-20250714-WA0007-1.jpg", "https://i.ibb.co/Y7JxZn9h/Untitled-1-5.jpg"]
+    images: [
+      "https://i.ibb.co/9k5Bn7T3/IMG-20250714-WA0007-1.jpg",
+      "https://i.ibb.co/Y7JxZn9h/Untitled-1-5.jpg"
+    ]
   },
   {
     name: "Swastik Rakhi",
     price: 49,
-    images: ["https://i.ibb.co/qYYrBn5P/IMG-20250714-WA0019.jpg", "https://i.ibb.co/yF05FMjG/IMG-20250719-184934776-HDR.jpg"]
+    images: [
+      "https://i.ibb.co/qYYrBn5P/IMG-20250714-WA0019.jpg",
+      "https://i.ibb.co/yF05FMjG/IMG-20250719-184934776-HDR.jpg"
+    ]
   },
   {
     name: "Stone-Studded Bro Rakhi",
     price: 45,
-    images: ["https://i.ibb.co/WNDwnFF7/IMG-20250719-195324622-HDR.jpg", "https://i.ibb.co/TDQFMkX9/IMG-20250719-195255349.jpg"]
+    images: [
+      "https://i.ibb.co/WNDwnFF7/IMG-20250719-195324622-HDR.jpg",
+      "https://i.ibb.co/TDQFMkX9/IMG-20250719-195255349.jpg"
+    ]
   }
 ];
 
@@ -145,9 +181,8 @@ document.getElementById("orderForm").addEventListener("submit", function (e) {
 
   const orderSummary = Object.entries(cart)
     .filter(([_, item]) => item.quantity > 0)
-    .map(([name, item]) =>
-      `${name} - ₹${item.price} × ${item.quantity} = ₹${item.price * item.quantity}`
-    ).join('\n');
+    .map(([name, item]) => `${name} - ₹${item.price} × ${item.quantity} = ₹${item.price * item.quantity}`)
+    .join('\n');
 
   const totalAmount = Object.values(cart)
     .reduce((total, item) => total + (item.price * item.quantity), 0);
